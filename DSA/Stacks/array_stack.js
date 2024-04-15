a5e3214099f376c0d8ea4551730e0ce4d4c56e67
sudipstha08 <sudipstha08@gmail.com>
2024-04-15 17:44:59 +0545

    dsa: array stack

---
+++ DSA/Stacks/array_stack.js
@@ -0,0 +1,26 @@
+/**
+ * Implementing stack using arrays for palindrome
+ * Palindrome - A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward. For example, "radar"
+ */
+
+let letters = [];
+let word = "racecar";
+
+let rword = "";
+
+// put letters of word into stack
+for (let i = 0; i < word.length; i++) {
+  // push on top of stack
+	letters.push(word[i]);
+}
+
+// pop off the stack in reverse order
+for (let i = 0; i < word.length; i++) {
+	rword += letters.pop();
+}
+
+if (rword === word) {
+	console.log(word + " is a palindrome");
+} else {
+	console.log(word + " is not a palindrome");
+}
