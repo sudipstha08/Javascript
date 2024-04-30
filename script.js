2b20f2b02e88b5d32c44017132a838711cef3f29
sudipstha08 <sudipstha08@gmail.com>
2024-04-30 14:16:04 +0545

    feat: modal

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const obj = {
-  count: 0,
-  handleClick: function() {
-      obj.count++;
-      console.log(this)
-      console.log("Button clicked:", this.count);
-  }
+	count: 0,
+	handleClick: function () {
+		obj.count++;
+		console.log(this);
+		console.log("Button clicked:", this.count);
+	},
 };
 
 // Bind the handleClick method to the obj object
 // const handleClickBound = obj.handleClick.bind(obj);
 
 // Attach the bound handleClick method as an event listener to the button
-const button2 = document.getElementById('button2');
-button2.addEventListener('click', obj.handleClick.bind(obj));
+const button2 = document.getElementById("button2");
+button2.addEventListener("click", obj.handleClick.bind(obj));
+
+// Modal
+
+const modal = document.querySelector("#modal");
+const openModalButton = document.querySelector("#open-modal-btn");
+const closeModalButton = document.querySelector("#close-modal-btn");
+const overlay = document.querySelector("#overlay");
+
+openModalButton.addEventListener("click", () => {
+	modal.classList.add("open");
+	overlay.classList.add("open");
+});
+
+const closeModal = () => {
+	modal.classList.remove("open");
+	overlay.classList.remove("open");
+};
+
+closeModalButton.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
